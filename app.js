d1dc2f32f577afaaf2f7310cb828de50b46c91c4
peter chege <peterchege442@gmail.com>
2020-03-20 22:52:32 +0300

    attack completed

--- app.js
+++ app.js
@@ -4,5 +4,47 @@ new Vue({
         playerHealth: 100,
         monsterHeath: 100,
         gameIsRunning: false
-    }
+    },
+    methods: {
+        startGame: function(){
+            this.gameIsRunning = true;
+            this.playerHealth = 100;
+            this.monsterHeath = 100;
+        },
+        attack: function(){
+            var max = 10;
+            var min = 3;    
+            var damage = Math.max(Math.floor(Math.random() * max) + 1, min)
+            this.monsterHeath -= damage;
+
+            if(this.monsterHeath <=0){
+                alert("You Won!");
+                this.gameIsRunning = false;
+                return;
+            }
+
+            var max = 12;
+            var min = 5;    
+            var damage = Math.max(Math.floor(Math.random() * max) + 1, min)
+            this.playerHealth -= damage;
+
+            if(this.playerHealth <=0){
+                alert("You Lost!");
+                this.gameIsRunning = false;
+                return;
+            }
+
+
+        },
+        specialAttack: function (){
+
+        },
+        heal:function(){
+
+        },
+        giveUp: function(){
+
+
+        }
+    },
 });
